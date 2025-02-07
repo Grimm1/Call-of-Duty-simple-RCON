4ab07ba10de656ba89ff99cd118b259828c66d98
Grimm1 <chrisellison50@hotmail.com>
2025-02-07 18:50:49 +0000

    Update header.php
    adding username stuff

--- header.php
+++ header.php
@@ -1,11 +1,20 @@
 <?php
-
-//sample code only
 include 'functions/menu.php';
 ?>
+
 <div class="container">
     <div id="branding">
-        
+    <?php
+    // Check if the user is logged in
+    if (isset($_SESSION['username']) && isset($_SESSION['role_name'])) {
+        $username = htmlspecialchars($_SESSION['username']);
+        $roleName = htmlspecialchars($_SESSION['role_name']);
+        echo "<div class='welcome-message' style='color: #4CAF50; font-size: 14px; float: right;'>Welcome, $username <span style='color: blue; font-size: 10px;'>($roleName)</span> </div>";
+
+
+
+    }
+    ?>   
     </div>
     <div class="buttons">
         <?php
